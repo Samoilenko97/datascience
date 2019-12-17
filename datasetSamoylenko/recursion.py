113026c8229e2b96966d2860af27f424643c028b
Samoilenko97 <56062471+Samoilenko97@users.noreply.github.com>
2019-12-17 02:48:23 +0200

    Update recursion.py

--- datasetSamoylenko/recursion.py
+++ datasetSamoylenko/recursion.py
@@ -1,11 +1,11 @@
 from dataset_structure import dataset
-def func2(dataset,n=0,keys_list=None):
+def er(dataset,a=0,keys_list=None):
     if keys_list==None:
         keys_list = list(dataset.keys())
         return func2(dataset,n,keys_list)
-    if n==len(dataset):
+    if a==len(dataset):
         return
-    if dataset[keys_list[n]]["percent_of_benefciaries_with_cancer"]>5 and dataset[keys_list[n]]["percent_of_beneficiaries_with_chf"]> 15 :
-        for elements in dataset[keys_list[n]]:
-            print(elements + ' - '+ str(dataset[keys_list[n]][elements]))
-    func2(dataset,n+1,keys_list)
+    if dataset[keys_list[a]]["percent_of_benefciaries_with_cancer"]>20 and dataset[keys_list[a]]["percent_of_beneficiaries_with_chf"]> 10 :
+        for elements in dataset[keys_list[a]]:
+            print(elements + ' - '+ str(dataset[keys_list[a]][elements]))
+    er(dataset,a+1,keys_list)
